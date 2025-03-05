9e72633d82b8608add2dacecccf816ebf932177f
maaniguard <maani@DESKTOP-JRK564J>
2025-03-05 22:29:45 +1100

    Section 2

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+        public required DbSet<Domain.Activity> Activities { get; set; }
+}
